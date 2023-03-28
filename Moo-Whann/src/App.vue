5069e25a4a02aa133a4ab0080919051e10d6106d
JastKherZ <jastkherz@hotmail.com>
2023-03-28 13:54:25 +0700

    Tailwin

---
+++ Moo-Whann/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="bg-blue-400 text-gray-100 py-3.5 px-6 shadow md:flex justify-between">
+    <navbar />
+  </div>
+</template>
+
+<script>
+import { createApp } from 'vue';
+import navbar from './components/navbar.vue';
+export default {
+  name: "App",
+  components: {
+    navbar,
+  },
+};
+</script>
