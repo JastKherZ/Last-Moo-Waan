c8af0fd259c5ee8779c458959177a143f3fe4ecd
PiyaChaiyanit <64070186@kmitl.ac.th>
2023-05-17 15:47:43 +0700

    add footer

--- Moo-Whann/src/App.vue
+++ Moo-Whann/src/App.vue
@@ -2,16 +2,19 @@
   <div>
     <navbar></navbar>
     <router-view></router-view>
+    <footerr></footerr>
   </div>
 </template>
 
 <script>
 import navbar from './components/navbar.vue';
+import footerr from './components/footer.vue';
 
 export default {
   name: "App",
   components: {
     navbar,
-},
+    footerr,
+}
 };
 </script>
