d38590195b763e9ce04f19c2cc0bdacf5241a5df
JastKherZ <jastkherz@hotmail.com>
2023-05-17 15:59:40 +0700

    update home page

--- Moo-Whann/src/views/home.vue
+++ Moo-Whann/src/views/home.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="flex flex-col justify-center items-center border">
+    <div class="flex flex-col justify-center items-center border bg-gray-100">
         <div class="text-center py-10">
             <h1 class="text-xl font-bold">พบกับร้าน MOOWHAN ใหม่ในปี 2023</h1>
             <p>ร้านค้าที่ขายทุกๆอย่างให้คุณ ดูแลและรักษาความปลอดภัยให้กับสินค้าของคุณ</p>
@@ -7,13 +7,13 @@
         </div>
 
         <!-- รูปร้าน -->
-        <div class="flex border">
+        <div class="flex">
             <img src="https://media.discordapp.net/attachments/595608417234845715/1108312674938408990/Screenshot_2566-05-17_at_15.38.46.png?width=2126&height=1404" class="h-1/6 max-w-full" alt="" />
         </div>
 
-        <!-- สินค้าตัวอย่าง -->
+        <!-- สินค้าขายดี -->
         <div class="text-center">
-            <p class="text-xl font-bold py-6">ตัวอย่างสินค้า</p>
+            <p class="text-xl font-bold py-6">สินค้าขายดีประจำเดือน</p>
             <div class="flex">
                 <div class="max-w-sm rounded overflow-hidden shadow-lg m-6">
                     <img class="w-full"
@@ -37,6 +37,19 @@
                         </p>
                     </div>
                 </div>
+
+                <div class="max-w-sm rounded overflow-hidden shadow-lg m-6">
+                    <img class="w-full"
+                        src="https://media.discordapp.net/attachments/595608417234845715/1097742119977898034/magic_pen.jpeg?width=448&height=448"
+                        alt="Sunset in the mountains">
+                    <div class="px-6 py-4">
+                        <div class="font-bold text-xl mb-2">ปากกาเมจิก</div>
+                        <p class="text-gray-700 text-base">
+                            ปากกาคุณภาพสูง เหมาะกับการทำงานทุกประเภท
+                        </p>
+                    </div>
+                </div>
+
             </div>
         </div>
     </div>
