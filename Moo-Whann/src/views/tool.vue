f88dbcf994ed0c1e4b751fd9d7acc9b5f1784c41
PiyaChaiyanit <64070186@kmitl.ac.th>
2023-05-17 16:23:38 +0700

    4 item per type

--- Moo-Whann/src/views/tool.vue
+++ Moo-Whann/src/views/tool.vue
@@ -24,6 +24,28 @@
                     </p>
                 </div>
             </div>
+            <div class="max-w-sm rounded overflow-hidden shadow-lg m-6">
+                <img class="w-full"
+                    src="https://media.discordapp.net/attachments/595608417234845715/1097742119977898034/magic_pen.jpeg?width=448&height=448"
+                    alt="Sunset in the mountains">
+                <div class="px-6 py-4">
+                    <div class="font-bold text-xl mb-2">ปากกาเมจิก</div>
+                    <p class="text-gray-700 text-base">
+                        ปากกาคุณภาพสูง เหมาะกับการทำงานทุกประเภท
+                    </p>
+                </div>
+            </div>
+            <div class="max-w-sm rounded overflow-hidden shadow-lg m-6">
+                <img class="w-full"
+                    src="https://media.discordapp.net/attachments/595608417234845715/1097742119977898034/magic_pen.jpeg?width=448&height=448"
+                    alt="Sunset in the mountains">
+                <div class="px-6 py-4">
+                    <div class="font-bold text-xl mb-2">ปากกาเมจิก</div>
+                    <p class="text-gray-700 text-base">
+                        ปากกาคุณภาพสูง เหมาะกับการทำงานทุกประเภท
+                    </p>
+                </div>
+            </div>
         </div>
     </div>
 </template>
