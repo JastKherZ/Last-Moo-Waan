9c0ca5dbba88f8daa77af5316ff0b0bd69ca26fc
JastKherZ <jastkherz@hotmail.com>
2023-05-21 01:01:26 +0700

    update edititem

--- Moo-Whann/src/views/edititem.vue
+++ Moo-Whann/src/views/edititem.vue
@@ -4,19 +4,21 @@
         <div class="font-bold text-center py-10">
             <h1 class="text-lg">edit item</h1>
         </div>
-        <div class="flex flex-col place-items-center border bg-red-500 h-[500px] w-[850px] mb-28">
+        <div class="flex flex-col border bg-red-500 h-[500px] w-[850px] mb-28">
            <!-- loop item -->
-            <select name="cars" id="cars" class="flex justify-center item-center bg-yellow-500 h-[100px] w-[400px] mt-5 mb-5">
-                <option value="volvo">Volvo</option>
-                <option value="saab">Saab</option>
-                <option value="mercedes">Mercedes</option>
-                <option value="audi">Audi</option>
-            </select>
-            
-                <div class="flex bg-orange-50 h-[40px] w-[70px]">
-                    <button class="text-center">Edit</button>
+           <div class="flex justify-center">
+               <select name="cars" id="cars" class="flex bg-yellow-500 h-[100px] w-[400px] mt-5 mb-5 ml-[28px]">
+                   <option value="volvo">Volvo</option>
+                   <option value="saab">Saab</option>
+                   <option value="mercedes">Mercedes</option>
+                   <option value="audi">Audi</option>
+               </select>
+           </div>
+            <div class="flex justify-end">
+                <div class="flex">
+                    <button class="bg-yellow-500 mr-5">Finish</button>
                 </div>
-            
+            </div>
         </div>
         
     </div>
