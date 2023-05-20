4ab679e6e2e855e3c09202b78b9e57510790147f
JastKherZ <jastkherz@hotmail.com>
2023-05-20 23:48:33 +0700

    finish basket

--- Moo-Whann/src/views/basket.vue
+++ Moo-Whann/src/views/basket.vue
@@ -1,12 +1,18 @@
 <template>
-    <div class="flex flex-col justify-center items-center border">
-        <div class="border">
-            <h1>SHOPPING BAG</h1>
+    <div class="flex flex-col border">
+        <div class="flex justify-center items-center my-8 border">
+            <h1 class="font-bold">SHOPPING BAG</h1>
         </div>
-        <div class="border">
+        <div class="flex justify-center items-center border my-4">
+            <p class="text-sm underline m-2">ตระกร้าสินค้า (number)</p>
+        </div>
+        <div class="flex flex-col border">
+            <p class="text-sm ml-2">คุณมีสินค้า number รายการอยู่ในตะกร้า</p>
             <router-link to="/">
-                <div class="space-x-5">
-                    <button class="text-sm">CONTINUE SHOPPING</button>
+                <div class="space-x-5 ml-2">
+                    <button class="bg-white font-bold border-4">
+                            CONTINUE SHOPPING
+                        </button>
                 </div>
             </router-link>
         </div>
