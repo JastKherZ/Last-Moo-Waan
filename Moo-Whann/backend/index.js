2ee3132baac55b4b02a27056770008ae55b1cc82
PiyaChaiyanit <64070186@kmitl.ac.th>
2023-05-21 21:34:09 +0700

    add register

--- Moo-Whann/backend/index.js
+++ Moo-Whann/backend/index.js
@@ -11,10 +11,12 @@ app.use(cors());
 
 // const userRouter = require('./routes/user')
 const itemRouter = require('./routes/item')
+const userRouter = require('./routes/user')
 // const paymentRouter = require('./routes/payment')
 
 // app.use(userRouter.router)
 app.use("/api/items", itemRouter.router)
+app.use("/api/user", userRouter.router) 
 // app.use(paymentRouter.router)
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
--- Moo-Whann/backend/routes/user.js
+++ Moo-Whann/backend/routes/user.js
@@ -1,13 +1,90 @@
-// const express = require("express")
+const express = require('express');
+const bcrypt = require('bcrypt');
+const Joi = require('joi');
+const { PrismaClient } = require('@prisma/client');
+const jwt = require('jsonwebtoken');
 
-// router = express.Router();
-
-
-
-
-
-
+const prisma = new PrismaClient();
 
 
+router = express.Router();
 
-// exports.router = router;
+const schema1 = Joi.object({
+    fname: Joi.string().required(),
+    lname: Joi.string().required(),
+    username: Joi.string().required(),
+    password: Joi.string().min(6).max(16).required(),
+    repassword: Joi.ref('password')
+  });
+
+
+router.get('/', async (req, res) => {
+
+    const result = await prisma.user.findMany()
+    res.json(result)
+})
+
+
+
+
+
+
+
+//register
+router.post('/register', async (req, res) => {
+  const qry = req.body;
+  
+  // Validate input using Joi
+  
+
+  const result = schema1.validate(qry);
+
+
+  // console.log(result);
+
+
+
+  if (result.error) {
+    // if (result.error.details[0].path[0] == 'fname'){
+    //   return res.status(400).send({ "error " : result.error.details[0].message})  
+    // }
+    // if (result.error.details[0].path[0] == 'lname'){
+    //   return res.status(400).send({ "error " : result.error.details[0].message})  
+    // }
+    // if (result.error.details[0].path[0] == 'username'){
+    //   return res.status(400).send({ "error " : result.error.details[0].message})  
+    // }
+    // if (result.error.details[0].path[0] == 'password'){
+    //   return res.status(400).send({ "error " : result.error.details[0].message})  
+    // }
+    // if (result.error.details[0].path[0] == 'repassword'){
+    //   return res.status(400).send({ "error " : result.error.details[0].message})  
+    // }
+    return res.status(400).send({ "error" :  result.error.details[0].message });
+  }
+
+  try {
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(qry.password, 10);
+
+    // Save the user to the database
+    const user = await prisma.user.create({
+      data: {
+        
+        fname : qry.fname,
+        lname : qry.lname,
+        username : qry.username,
+        password: hashedPassword,
+        
+      }
+    });
+
+    res.json({ message: 'User registered successfully', user });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Something went wrong' });
+  }
+});
+
+
+exports.router = router;
