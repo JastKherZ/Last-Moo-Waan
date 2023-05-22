a900984593c362e9250ede2f45a50889909e14f2
JastKherZ <jastkherz@hotmail.com>
2023-05-22 13:49:30 +0700

    fix login

--- Moo-Whann/src/views/login.vue
+++ Moo-Whann/src/views/login.vue
@@ -3,33 +3,38 @@
         <div class="font-bold text-center py-4">
             <h1 class="text-lg">SIGN IN</h1>
         </div>
-        <div class="flex justify-between border h-[400px] w-[450px]">
-            <div class="flex space-x-8 pt-6">
-                <div class="flex flex-col">
-                    <h1 class="font-bold text-sm pb-6">RETURNING CUSTOMERS</h1>
-                    <label for="username">Username</label>
-                    <input type="text" class="" v-model="username">
-                    <label for="password">Password</label>
-                    <input :type="password" v-model="password">
-                    <div class="flex">
-                        <input type="checkbox" @click="toggleShow">
-                        <label for="">Show Password</label>
-                    </div>
-                    <button class="bg-black text-white font-bold">
-                        <a @click.prevent="validateForm">SIGN IN</a>
+        <div class="flex">
+            <div class="flex flex-col my-12">
+                <h1 class="font-bold text-sm pb-6">RETURNING CUSTOMERS</h1>
+                <label for="username" class="text-sm">Username</label>
+                <input type="text" class="px-2 border rounded text-sm mt-1 w-64 h-8 italic"
+                    placeholder="Please enter your Username" v-model="username">
+                <label for="password" class="text-sm mt-1">Password</label>
+                <input :type="password" class="px-2 border rounded text-sm mt-1 w-64 h-8 italic"
+                    placeholder="Please enter your Password" v-model="password">
+                <div class="flex mt-1">
+                    <input type="checkbox" @click="toggleShow">
+                    <label for="" class="mx-2 text-sm">Show Password</label>
+                </div>
+                <button class="bg-black text-white font-bold mt-28 h-10 w-64 border rounded">
+                    <a @click.prevent="validateForm">SIGN IN</a>
+                </button>
+            </div>
+
+            <div class="flex bg-gray-500 w-[1px] h-[360px] mx-16 mt-12">
+            </div>
+
+            <div class="flex flex-col my-12">
+                <h1 class="font-bold text-sm pb-6">CREATE AN ACCOUNT</h1>
+                <label for="" class="items-center text-sm mb-2">สมัครสมาชิคกับ Moo Whan <br> และรับสิทธิประโยชน์ของการมีบัญชี:</label>
+                <li class="px-2 text-sm">ติดตามข่าวสารใหม่ๆ รวดเร็วทันใจ</li>
+                <li class="px-2 text-sm">เช็คประวัติการสั่งซื้อและดูข้อมูลสินค้า</li>
+                <li class="px-2 text-sm">ดูสินค้าได้แม้จะอยู่ที่ไหนก็ตาม</li>
+                <router-link to="/register">
+                    <button class="bg-black text-white font-bold mt-36 h-10 w-64 border rounded">
+                        CREATE AN ACCOUNT
                     </button>
-                </div>
-                <div class="bg-gray-500 w-[1px] h-[150px]">
-                </div>
-                <div>
-                    <h1 class="font-bold text-sm pb-2.5">CREATE AN ACCOUNT</h1>
-                    <label for="">ยังไม่มีสมาชิกใช่ไหม</label>
-                    <div class="flex flex-col pt-28">
-                        <button class="bg-white font-bold">
-                            <router-link to="/register">CREATE AN ACCOUNT</router-link>
-                        </button>
-                    </div>
-                </div>
+                </router-link>
             </div>
         </div>
 
@@ -37,7 +42,6 @@
 </template>
 
 <script>
-import axios from 'axios'
 export default {
     data() {
         return {
@@ -57,22 +61,21 @@ export default {
         },
         validateForm() {
             if (this.username == '') {
-                alert('Please enter a username');
+                alert('Please enter your Username');
                 console.log(this.username);
                 return false;
             }
 
             else if (this.password == '') {
-                alert('Please enter a password');
+                alert('Please enter your Password');
                 console.log(this.password);
                 return false;
             }
-            else if (this.password != '123123123') {
-                alert('Wrong Password!')
-            }
             else {
                 this.$router.push("/")
-            } 
+            }
+
+            return true;
         }
     }
 };
