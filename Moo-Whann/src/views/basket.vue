2d0647bd409ecdba62eabf9d7a830b246012e2fc
JastKherZ <jastkherz@hotmail.com>
2023-05-22 14:05:10 +0700

    fix login

--- Moo-Whann/src/views/basket.vue
+++ Moo-Whann/src/views/basket.vue
@@ -1,10 +1,11 @@
 <template>
-    <div class="flex flex-col border">
-        <div class="flex justify-center items-center my-8 border">
-            <h1 class="font-bold">SHOPPING BAG</h1>
+    <div class="flex flex-col items-center border bg-gray-100 h-[800px] w-full">
+        <div class="flex flex-col items-center my-8 border">
+            <h1 class="text-xl font-bold">SHOPPING BAG</h1>
+            <label for="" class="text-sm">ตะกร้าสินค้า (number)</label>
         </div>
         <div class="flex justify-center items-center border my-4">
-            <p class="text-sm underline m-2">ตระกร้าสินค้า (number)</p>
+            <p class="text-sm underline m-2">ตะกร้าสินค้า (0)</p>
         </div>
         <div class="flex flex-col border">
             <p class="text-sm ml-2">คุณมีสินค้า number รายการอยู่ในตะกร้า</p>
--- Moo-Whann/src/views/login.vue
+++ Moo-Whann/src/views/login.vue
@@ -1,7 +1,7 @@
 <template>
-    <div class="flex flex-col justify-center items-center border bg-gray-100 h-[800px] w-full">
+    <div class="flex flex-col items-center bg-gray-100 h-[800px] w-full">
         <div class="font-bold text-center py-4">
-            <h1 class="text-lg">SIGN IN</h1>
+            <h1 class="text-lg mt-16">SIGN IN</h1>
         </div>
         <div class="flex">
             <div class="flex flex-col my-12">
@@ -9,10 +9,10 @@
                 <label for="username" class="text-sm">Username</label>
                 <input type="text" class="px-2 border rounded text-sm mt-1 w-64 h-8"
                     placeholder="Please enter your Username" v-model="username">
-                <label for="password" class="text-sm mt-1">Password</label>
+                <label for="password" class="text-sm mt-4">Password</label>
                 <input :type="password" class="px-2 border rounded text-sm mt-1 w-64 h-8"
                     placeholder="Please enter your Password" v-model="password">
-                <div class="flex mt-1">
+                <div class="flex mt-4">
                     <input type="checkbox" @click="toggleShow">
                     <label for="" class="mx-2 text-sm">Show Password</label>
                 </div>
@@ -31,7 +31,7 @@
                 <li class="px-2 text-sm">เช็คประวัติการสั่งซื้อและดูข้อมูลสินค้า</li>
                 <li class="px-2 text-sm">ดูสินค้าได้แม้จะอยู่ที่ไหนก็ตาม</li>
                 <router-link to="/register">
-                    <button class="bg-black text-white font-bold mt-36 h-10 w-64 border rounded">
+                    <button class="bg-black text-white font-bold mt-[167px] h-10 w-64 border rounded">
                         CREATE AN ACCOUNT
                     </button>
                 </router-link>
